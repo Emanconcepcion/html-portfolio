5b595f09e73c7931a87835a3743cb5a7fa2ebf13
Eman Concepcion <eman_concepcion@yahoo.com>
2022-05-17 02:18:12 -0700

    adding screenshot, adding favcons and fixing testimonials

--- README.md
+++ README.md
@@ -14,9 +14,9 @@ I have created a portfolio that displays information about me, my most recent pr
 
 ## Technologies
 
-- ![](./assets/icons/icons8-html-5.svg)
-- ![](./assets/icons/icons8-css3.svg)
-- ![](./assets/icons/icons8-javascript.svg)
+- ![](./icons/icons8-html-5.svg)
+- ![](./icons/icons8-css3.svg)
+- ![](./icons/icons8-javascript.svg)
 
 ## Installation
 
@@ -28,3 +28,9 @@ This is a webpage and it does not require any installation. To visit the website
 - [Website](https://emanconcepcion.github.io/personal-porfolio/)
 
 ## Screenshots
+
+![](./icons/capture1.PNG)
+![](./icons/capture2.PNG)
+![](./icons/capture3.PNG)
+![](./icons/capture4.PNG)
+![](./icons/capture5.PNG)
--- testimonials.js
+++ testimonials.js
@@ -3,28 +3,28 @@ const testimonials = [
     quote:
       "our company, we do ongoing research with our target audience. This includes 30-45 minute phone interviews. It was difficult to conduct the interview, really listen, and ask good follow up questions.",
     avatar: "avatar1",
-    name: "Diana Ayi",
-    title: "Musician",
+    name: "Ling Ying Wei",
+    title: "Sentinel",
   },
   {
     quote:
       "our company, we do ongoing research with our target audience. This includes 30-45 minute phone interviews. It was difficult to conduct the interview, really listen, and ask good follow up questions.",
     avatar: "avatar2",
-    name: "Ernest Achiever",
-    title: "Web Developer",
+    name: "Sasha Novikov",
+    title: "Initiator",
   },
   {
     quote:
       "our company, we do ongoing research with our target audience. This includes 30-45 minute phone interviews. It was difficult to conduct the interview, really listen, and ask good follow up questions.",
     avatar: "avatar3",
-    name: "Asamoah Gyan",
-    title: "Footerballer",
+    name: "Vincent Fabron",
+    title: "Sentinel",
   },
   {
     quote:
       "our company, we do ongoing research with our target audience. This includes 30-45 minute phone interviews. It was difficult to conduct the interview, really listen, and ask good follow up questions.",
     avatar: "avatar4",
-    name: "Beatrice Aku",
-    title: "Politician",
+    name: "Sunwoo Han",
+    title: "Duelist",
   },
 ];
